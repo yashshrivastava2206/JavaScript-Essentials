c219b9aeeeb7996050960adf971e436651c48cc4
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-03-30 14:26:41 +0530

    Add files via upload

---
+++ VarPreType.js
@@ -0,0 +1,47 @@
+//Variable Declaration
+
+let name= "Yash";
+console.log(name);
+name = "Nishu";
+console.log(name);
+name =21;
+console.log(name);
+
+// Constant Variable Declaration
+
+const Age=12;
+console.log(Age);
+//Age =29; //Not Allowed
+
+// Primitive Data Types 
+
+let studentNam="Jay Shrivastava";
+let studedntAge= 21;
+let studentPercent =77.85;
+let website ="www.google.com";
+let Choice =true; //Boolean has two values- true and false
+let time;
+console.log(time); // Return Undefined (Default)
+let date=null;
+console.log(date); //null is used to say that there is no value
+
+//Sum of two numbers
+
+let num1=21;
+let num2= 40;
+let num3= num1+num2;
+console.log(num3);
+
+//Concatination Of String
+
+let fname= "Yash";
+let lname= "Shrivastava";
+let fullName =fname + " " + lname ;
+console.log(fullName);
+
+//Concatination
+
+let Name = "Aman";
+let age = 21;
+let detail= Name + " is " +age+ " years old";
+console.log(detail);
