4df858f45c48f21a4a65dcd105eab2c2dd406496
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-09-06 10:23:30 +0530

    Add files via upload

---
+++ ForLoop.js
@@ -0,0 +1,69 @@
+let str="";
+for(let i=0;i<5;i++){
+    str=str+'A';
+    console.log(str);
+}
+
+let num="";
+for(let j=1;j<=5;j++){
+    num=num+j;
+    console.log(num);
+}
+
+for(i=1;i<=10;i++){
+    let mult= 3*i;
+    str2="3*"+i+"="+mult;
+    console.log(str2);
+}
+
+//Tash 4
+
+let str3="EmployeeID";
+for(let k=1;k<6;k++){
+    let str= str3+"::"+k;
+    console.log(str);
+}
+
+//Task 5:
+
+for(let l=1;l<=11;l++){
+    let str= "Number "+l+", Cube "+(l*l*l);
+    console.log(str);
+}
+
+//Task 6:
+let str5="";
+for(let a=1;a<=10;a++){
+    str5=a+" "+(a*10)+" "+(a*20) ;
+    console.log(str5);
+}
+
+//Task 7:
+for(i=1;i<=8;i++){
+    str=i+" 1"+i+" 10"+i+" 100"+i;
+    console.log(str);
+}
+
+//Task 8:
+
+let n=5;
+for(i=1;i<=n;i++){
+    str="X"+i;
+    console.log(str);
+}
+
+//Task 11:
+str="";
+n=3;
+for(i=1;i<=n;i++){
+    str=str+"A"+i;
+    console.log(str);
+}
+
+//Task 12:
+
+n=4;
+for(i=1;i<=n;i++){
+    str=i+"+"+(i+1)+"="+(i*2+1);
+    console.log(str);
+}
